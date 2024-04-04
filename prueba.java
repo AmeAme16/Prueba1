6e1f77328fd39221a4cb714a5836a46bbaa6e0e3
Amelia <117388052+AmeAme16@users.noreply.github.com>
2024-04-04 16:20:46 +0200

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,9 +1,10 @@
 public class prueba {
     String nombre;
     int num;
-
+    boolean cierto;
     public prueba(String nombre, int num) {
         this.nombre = nombre;
         this.num = num;
     }
+    
 }
